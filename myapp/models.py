b8c48dd51b8e632e898c8920a0482bc82e6d5898
William Matallana <willipro1804@gmail.com>
2023-08-10 06:22:27 -0500

    hola

--- myapp/models.py
+++ myapp/models.py
@@ -4,13 +4,14 @@ class Project(models.Model):
     '''
     Modelo que representa un proyecto
     '''
-
-    name = models.CharField(max_length=50, help_text='ingrese el nombre del proyecto')
+    name = models.CharField(max_length=50, help_text= 'Ingrese el nombre del proyecto')
 
 class Task(models.Model):
+
     '''
-    Modelo que representa una tarea de un proyecto
+    Modeo que representa una tarea de un proyecto
     '''
-    title = models.CharField(max_length=200, help_text='ingrese el titulo de la tarea')
-    description = models.TextField(help_text='ingrese la descripción de la tarea')
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=200, help_text='Ingrese el titulo de la tarea')
+    description = models.TextField(help_text='Ingrese la descripcion de la tarea')
+    Project = models.ForeignKey(Project, on_delete=models.CASCADE)
